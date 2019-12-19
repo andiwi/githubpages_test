bce1b559e0cb2e7c02329359f339dafbf3a16065
Andreas Wittmann <mail@andreaswittmann.com>
2019-12-19 19:34:30 +0100

    Updates

---
+++ precache-manifest.bd7c510a0571285938cfcbd83f78beaf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "883c510d51d115acab7c703a32478e12",
+    "url": "/githubpages_test/index.html"
+  },
+  {
+    "revision": "f5306972c38e720e763a",
+    "url": "/githubpages_test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "27afab926d624157c76c",
+    "url": "/githubpages_test/static/js/2.c0c3c25a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/githubpages_test/static/js/2.c0c3c25a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5306972c38e720e763a",
+    "url": "/githubpages_test/static/js/main.e06c0341.chunk.js"
+  },
+  {
+    "revision": "cfbb5c6154006d89a565",
+    "url": "/githubpages_test/static/js/runtime-main.bc3c075b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubpages_test/static/media/logo.5d5d9eef.svg"
+  }
+]);
